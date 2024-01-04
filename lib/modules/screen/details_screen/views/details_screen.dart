f43bc34abe267bcac285f25730be5b71220d9d09
HP <radadiyakrish62@gmail.com>
2024-01-04 10:22:07 +0530

    intro screen

--- lib/modules/screen/details_screen/views/details_screen.dart
+++ lib/modules/screen/details_screen/views/details_screen.dart
@@ -70,7 +70,6 @@ class Home_page extends StatelessWidget {
                           MaterialStateProperty.all(const Color(0xFFFEF99D)),
                     ),
                     onPressed: () {
-                      guj = !guj;
                       Navigator.of(context).pushNamed('details');
                     },
                     child: const Text(
@@ -88,8 +87,6 @@ class Home_page extends StatelessWidget {
                           MaterialStateProperty.all(const Color(0xFFFEF99D)),
                     ),
                     onPressed: () {
-                      eng = !eng;
-
                       Navigator.of(context).pushNamed('details');
                     },
                     child: const Text(
@@ -107,8 +104,6 @@ class Home_page extends StatelessWidget {
                           MaterialStateProperty.all(const Color(0xFFFEF99D)),
                     ),
                     onPressed: () {
-                      hindi = !hindi;
-
                       Navigator.of(context).pushNamed('details');
                     },
                     child: const Text(
@@ -135,82 +130,17 @@ class Home_page extends StatelessWidget {
               //     ),
               //   ),
               // ),
-              child: Center(
-                child: Text(
-                  '${jss.hi}',
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+              // child: Center(
+              //   child: Text(
+              //     '${jss.guj}',
+              //     style: const TextStyle(
+              //       fontSize: 20,
+              //       fontWeight: FontWeight.bold,
+              //     ),
+              //   ),
+              // ),
               //   child: Text('${json.san}'),
             ),
-            (eng == true)
-                ? Container(
-                    margin: EdgeInsets.all(10),
-                    height: 200,
-                    width: double.infinity,
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        image: DecorationImage(
-                            image: NetworkImage(
-                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB_KiOPWBICa6yQw4FmJ6t3BaA27gjnf_DmnfKb3aKqA9RLBlSun7L2MLN3iMlAmLFqp4&usqp=CAU"),
-                            fit: BoxFit.cover,
-                            opacity: 0.75)),
-                    child: Center(
-                      child: Text(
-                        jss.en,
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ),
-                  )
-                : Container(),
-            (hindi == true)
-                ? Container(
-                    margin: EdgeInsets.all(10),
-                    height: 200,
-                    width: double.infinity,
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB_KiOPWBICa6yQw4FmJ6t3BaA27gjnf_DmnfKb3aKqA9RLBlSun7L2MLN3iMlAmLFqp4&usqp=CAU"),
-                          fit: BoxFit.cover,
-                          opacity: 0.75),
-                    ),
-                    child: Center(
-                      child: Text(
-                        jss.hi,
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ),
-                  )
-                : Container(),
-            (guj == true)
-                ? Container(
-                    margin: EdgeInsets.all(10),
-                    height: 200,
-                    width: double.infinity,
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25),
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB_KiOPWBICa6yQw4FmJ6t3BaA27gjnf_DmnfKb3aKqA9RLBlSun7L2MLN3iMlAmLFqp4&usqp=CAU"),
-                          fit: BoxFit.cover,
-                          opacity: 0.75),
-                    ),
-                    child: Center(
-                      child: Text(
-                        jss.guj,
-                        style: TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ),
-                  )
-                : Container(),
           ],
         ),
       ),
